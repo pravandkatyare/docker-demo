fae557ad835dabed01d88446d8c4b93edabd8dd9
pravandkatyare <36467951+pravandkatyare@users.noreply.github.com>
2019-05-12 17:15:34 +0530

    code changes
    code changes

--- src/main/java/com/example/dockerdemo/controller/HomeController.java
+++ src/main/java/com/example/dockerdemo/controller/HomeController.java
@@ -4,11 +4,11 @@ package com.example.dockerdemo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/home")
+@RestController
 public class HomeController {
 
-    @GetMapping
-    public String home(){
-        return "Pravand";
+    @GetMapping(value = "/{name}")
+    public String home(@PathVariable String name){
+        return name;
     }
 }
